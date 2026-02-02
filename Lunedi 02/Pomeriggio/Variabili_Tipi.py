a90c8800e22d1ceddf84dac24215008d19c8ce89
mariavisone <mcvisone@virgilio.it>
2026-02-02 17:55:41 +0100

    Add files via upload

---
+++ Lunedi 02/Pomeriggio/Variabili_Tipi.py
@@ -0,0 +1,48 @@
+nome = "maria"
+età = 28
+print(nome, età)
+print ("questo è il mio nome:" , nome, "questa è la mia età:" , età)
+
+nome = input("inserisci il tuo nome: ")
+età = int( input("inserisci la tua età: "))
+print ("Ciao, " + nome + "! Benvenuto in Python!")
+
+print(1 + 6)
+print (6 - 1)
+print(4 / 2)
+print(2 ** 3)
+print(3 * 2)
+
+# prova concatenazione stringhe
+nome = "maria"
+saluto = "ciao"
+print( nome + " " + saluto)
+
+# questo è un esempio
+x = 1
+y = 0.2
+z = x - y
+print(x - z)
+
+s = "Python"
+print(s[0])
+print(s[2])
+
+s = "Ciao, phyton!"
+print(len(s))
+print(s.upper())
+print(s.split(','))
+print(s.replace('mondo', 'universo'))
+
+carattere = 'M'
+
+booleanT = True
+booleanF = False
+
+print( booleanT, booleanF )
+
+booleanT =int(True)
+booleanF =int(False)
+print( booleanT, booleanF)
+
+
