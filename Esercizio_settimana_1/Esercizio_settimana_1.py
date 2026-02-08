62a286b4eee18edec5e3df4594b8a0089b31870a
mariavisone <mcvisone@virgilio.it>
2026-02-08 20:16:04 +0100

    Add files via upload

--- Esercizio_settimana_1/Esercizio_settimana_1.py
+++ Esercizio_settimana_1/Esercizio_settimana_1.py
@@ -3,26 +3,29 @@
 nome = input("inserisci il tuo nome: ")
 print("Ciao, ", nome , "! Benvenuta in Python!")
 
-#chiedi all'utente la sua età e quella di suo fratello. poi stampale e addizionale (somma con int)
+#chiedi all'utente la sua età e quella di suo padre. poi stampale e addizionale (somma con int)
 etàM = int(input("la tua età è: "))
-etàF = int(input("l'eta di tuo fratello è: "))
-somma = etàM + etàF
-print(somma) 
- 
+etàP = int(input("l'eta di tuo padre è: "))
+somma = etàM + etàP
+print(somma , "è la somma della tua età e quella di tuo padre, curioso!")
+
 #I BOOLEANI con confronto
 #chiedi l'altezza del''utente. 
 #crea una variabile booleana che vale: 
 #true se l'altezza supera o uguale a 170cm altrim falsa
 
 altezza = int(input("inserisci la tua altezza: "))
+sei_alta = "Complimenti superi la media italiana!"
 sei_alta = altezza >= 170 #la mia variabile booleana con una condizione 
-print(sei_alta + "Complimenti superi la media italiana!")
+print(sei_alta)
+
 
 #collezioni - lista- insiemi -tuple
 #stampa una lista con i dati dell'utente raccolti fin ora.
 #chiedi all'utente se vuole aggiungere altro oltre alle info già date da porre nel sistema
 
 lista = [nome, etàM, altezza ]
+print("i dati aggiunti al sistema sono: ", lista)
 #if-elif-else
 richiesta = input("vuoi aggiungere qualcosa alla lista da aggiungere al tuo cv?si / no")
 if richiesta == "si":
@@ -63,12 +66,15 @@ else:
 
 #anni_lavorativi_per_pensione = 40
 pensione = input ("vuoi sapere quanti anni ti mancano alla pensione? si / no")
+info4 = int(input("anni di esperienza lavorativa: "))
 if pensione == "si":
-    conteggio = info4 
+    conteggio = info4 + 1
     while conteggio < 40:
         print (conteggio)
         conteggio += 1
-print ("siccome ti servono 40 anni di contributi alla pensione, sopra troverai conteggiati gli anni rimanenti")
+    print ("siccome ti servono 40 anni di contributi alla pensione, sopra troverai conteggiati gli anni rimanenti")
+else:
+    print("Ok proseguiamo.")
 
 certificati = int(input("quanti certificati vuoi inserire nel tuo cv?"))
 for i in range(1, certificati + 1, 1):
@@ -76,13 +82,15 @@ for i in range(1, certificati + 1, 1):
     tipo = input("tipo di certificato: ")
     anno = input("anno: ")
 
+info3 = input("inserisci la tua residenza: ")
+print("Ecco un recap dei dati inseriti a sistema per il tuo profilo")
 modella = [nome, etàM, altezza, info3, info4]
 
 def stampa_persona(modella):
     print("Nome:", modella[0])
     print("Età:", modella[1])
     print("Altezza:", modella[2])
-    print("Residenza", modella[3])
+    print("Residenza:", modella[3])
     print("Anni di esperienza:", modella[4])
 
 stampa_persona(modella)
