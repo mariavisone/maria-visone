d2907fb58c19a8c2d5c98366d1e3a6c088c83ce6
mariavisone <mcvisone@virgilio.it>
2026-02-12 15:31:59 +0100

    eserc.polimorfismo2.py
    da completare il main

--- Corso Python/Giovedi 12/Matttina/eserc.polimorfismo2.py
+++ Corso Python/Giovedi 12/Matttina/eserc.polimorfismo2.py
@@ -21,12 +21,14 @@ stampa_posti_occupati(): stampa tutti i posti che risultano occupati'''
 
  #creo una classe base Posto
 class Posto:
-    def __init__ (self, numero, fila):
-        self._numero = numero
-        self._fila = fila
-        self._occupato = False #all'inizio il posto è libero
-
-#scirvi un metodo per prenotare 
+    def __init__ (self, numero, fila): #creo gli attributi privati del posto
+        self._numero = numero   #numero del posto
+        self._fila = fila       #fila
+        self._occupato = False    #all'inizio il posto è libero
+    def __str__(self):
+        print("il posto", self._numero, "in fila", self._fila, "è ", self._occupato)
+        
+#scirvi un metodo per prenotare il posto
     def prenota(self):
         if self._occupato == False: #se il posto è libero
             print("il posto" , self._numero,"in fila", self._fila, "è libero")
@@ -41,18 +43,18 @@ class Posto:
             print("il posto" , self._numero, "in fila", self._fila, "è stato liberato")
 
 #adesso utilizzo il getter per recuperare il numero, la fila e lo stato (occupato/libero).
-    def get_numero(self):   #con il get recupero gli attributi senza modificarli
-        return self._numero
-    def get_fila(self):
-        return self._fila
-    def get_occupato(self):
-        return self._occupato
+    def get_numero(self):   #con il get recupero gli attributi senza modificare nulla
+        return self._numero  #restituisce il numero del posto
+    def get_fila(self):      
+        return self._fila     #restituisce il numero di fila del posto
+    def get_occupato(self): 
+        return self._occupato  #restituisce lo stato di occupazione del posto e cioè false se è libero e true se è occupato
 
 #classi derivate: postovip e postostandard
 class PostoVip(Posto):
-    def __init__(self, numero, fila, servizi_extra):
+    def __init__(self, numero, fila, servizi_extra): 
         super().__init__(numero, fila) #richiamo il costruttore della superclasse posto
-        self.servizi_extra = servizi_extra 
+        self.servizi_extra = servizi_extra #attributo aggiunto
 
 #ora sovrascrivo il metodo prenota
     def prenota(self):
@@ -76,7 +78,7 @@ class PostoStandard(Posto):
 
 class Teatro:
     def __init__ (self):
-        self._posti = []
+        self._posti = [] #questa lista contiene oggetti di tipo posto, postovip e posto standard
     #metodo aggiungi_posto(posto) per aggiungere un nuovo posto alla lista
     def aggiungi_posto (self, posto):
         self._posti.append(posto) #sto aggiungendo il posto alla lista vuota _posti
@@ -86,10 +88,45 @@ class Teatro:
             if posto.get_numero() == numero and posto.get_fila() == fila: #se lo trovo 
                 posto.prenota() #in tal caso prenoto il posto riprendendo il metodo prenota sopra 
                 return #per uscire dal metodo
-    def stampa_posti_occupati(): #per stampare tutti i  posti occupati nel teatro
-    #se io ho una lista self._posti dove si trovano i posti prenotati, perche quella list ala creo con i posti che ci inserisco ogni volta che prenoto, allora batsa pritnare quella lista ?! no uso la sunfione get_occupati
+    def stampa_posti_occupati(self): #per stampare tutti i  posti occupati nel teatro
+    #se io ho una lista self._posti dove si trovano tutti i posti standard e vip- ogni oggetto di questa lista ha il metodo get_occupato. 
+    #quindi creo un ciclo con for per controllare tutti i posti 
         for posto in self._posti: #per ogni posto nella lista self.posti
-            #uso def get_occupato(self) che ancora devo usare e che mi fa effettivamente i posti occupati
-            if posto  get_occupato() 
+            #uso def get_occupato(self) che mi fa capire se effettivamente i posti sono  occupati
+            if posto.get_occupato(): #se il posto è occupato
+                print("il posto: ", posto.get_numero(),"in fila: ", posto.get_fila())
 
-            
+#main
+
+
+
+san_carlo = Teatro()  #creo un teatro e lo chiamo san_carlo
+
+#creo due posti 
+posto1 = PostoStandard (19, "D",15) #numero, fila, costo
+posto2 = PostoVip (3, "B", "Accesso al Lounge" )
+
+#aggiungo i posti al teatro
+san_carlo.aggiungi_posto(posto1)
+san_carlo.aggiungi_posto(posto2)
+
+#prenota posti
+san_carlo.prenota_posto(19, "D")
+san_carlo.prenota_posto(3, "B")
+
+san_carlo.stampa_posti_occupati()
+
+"""devi rivedere la parte di servizi extra e fare in modo che sia una lista."""
+#per rendere il sistema ripetibile:
+
+while True:
+    print("1. prenota posto")
+    print("2. Libera posto")
+    print("3. Mostra i posti occupati")
+    print("4. esci")
+
+    scelta = input("scegli un numero: ")
+
+    if scelta == 1: #prenota posto
+        numero = int(input("nuemero posto scelto: "))
+        fila = input("Fila: ")
