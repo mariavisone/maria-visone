1c9879d4c6ee816dd8a9fb37d37a618040cdb6ac
mariavisone <mcvisone@virgilio.it>
2026-03-15 17:15:43 +0100

    Esercizi_completi.py

---
+++ Corso Python/Mercoledi_04_02/Pomeriggio/Esercizi completi.py
@@ -0,0 +1,27 @@
+#1 scrivi un sistema che prende in input un numero e stampa "Pari" se il numero è pari e "Dispari" se il numero è dispari.
+scelta = int( input("Inserisci un numero: "))
+if scelta % 2:
+    print("Pari")
+else: 
+    print("Dispari")
+
+
+
+#2 scrivi un sistema che prende in input ..
+
+n = int(input ("Inserisci un numero intero positivo: "))
+while True:
+    while n >= 0 :
+        print(n)
+        n -= 1
+    x = input("vuoi continuare?")
+    if x == "no":
+        break
+
+#3 .. ?
+lista = int( input["aggiungi una lista dei tuoi numeri preferiti: "])
+for i in lista:
+    print( i ** 2)
+
+#4 ..
+
