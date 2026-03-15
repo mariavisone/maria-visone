630c594e0f9ca6f143d9616681de595bbaf2825f
mariavisone <mcvisone@virgilio.it>
2026-03-15 17:17:28 +0100

    Cicli.py

---
+++ Corso Python/Mercoledi_04_02/Cicli.py
@@ -0,0 +1,39 @@
+# cicli
+"""
+conteggio = 0
+
+while conteggio < 10:
+    print(conteggio)
+    conteggio += 1
+
+#ciclo boleano
+controllore = True
+
+while controllore: 
+    print("oh no")
+
+    esci = input("Vuoi uscire- SI - NO")
+    if esci.lower() == "si":
+        controllore = False
+    else:
+        controllore = True
+else:
+    print("il controllore era false") """
+
+# for
+numeri = [1, 2, 3, 4, 5]
+
+for numero in numeri:
+    print(numero)
+
+# altro esempio per for
+
+for i in numeri:
+    print(i)
+
+for i in range(5):
+    print(i)
+
+for i in range (0, 21, 2):
+    print(i)
+
