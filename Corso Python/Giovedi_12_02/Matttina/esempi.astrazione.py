51060325e2cbdfddbb1561e346d6b0450774a521
mariavisone <mcvisone@virgilio.it>
2026-03-15 17:41:54 +0100

    esempi_astrazione.py

---
+++ Corso Python/Giovedi_12_02/Matttina/esempi.astrazione.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Animale (ABC):
+    @abstractmethod
+    def muovi(self):
+        pass
+
+class Cane(Animale):
+    def muovi(self):
+        print("Corro")
+    
+class Pesce(Animale):
+    def muovi(self):
+        print("Nuoto")
+        
